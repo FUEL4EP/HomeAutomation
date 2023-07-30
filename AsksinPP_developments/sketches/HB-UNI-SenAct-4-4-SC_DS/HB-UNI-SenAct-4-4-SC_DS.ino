6f5bbc5f15710108f4617e07e062c51f5c778e4d
FUEL4EP <epre@unitybox.de>
2023-07-30 14:55:48 +0200

    HB-UNI-SenAct-4-4-SC_DS: code update in order to allow to set the Arduino's releay control outputs to iinput mode when releys are off; this avoids potential leakage currents

--- AsksinPP_developments/sketches/HB-UNI-SenAct-4-4-SC_DS/HB-UNI-SenAct-4-4-SC_DS.ino
+++ AsksinPP_developments/sketches/HB-UNI-SenAct-4-4-SC_DS/HB-UNI-SenAct-4-4-SC_DS.ino
@@ -2,7 +2,7 @@
 // AskSin++
 // 2016-10-31 papa Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
 // 2018-08-13 jp112sdl Creative Commons - http://creativecommons.org/licenses/by-nc-sa/3.0/de/
-// 2023-07-07 FUEL4EP adaptions to PCB HB-UNI-SenAct-4-4-SC_DS_FUEL4EP 
+// 2023-07-30 FUEL4EP adaptions to PCB HB-UNI-SenAct-4-4-SC_DS_FUEL4EP 
 // special thanks to jp112sdl for the base version of HB-UNI-SenAct-4-4
 //- -----------------------------------------------------------------------------------------------------------------------
 // ci-test=yes board=328p aes=no
@@ -14,16 +14,12 @@
 // The device can be attached to a main supply with a demand switch (in German: Netzfreischalter), e.g. in a sleeping room
 // The software for charging the accumulator batteries is not yet implemented, please charge the accumulator batteries manually for the time being!
 
-
-
 // derived from https://github.com/jp112sdl/HB-UNI-SenAct-4-4 (C) jp112sdl
 // additional functionality for the hybrid charge mode will be added later on; currently they are not yet implemented in software, but only in hardware
 // the class template derived_SwitchChannel is inherited from the class template SwitchChannel of AsksinnPP. Some overloading is done for the required new functionality.
 
 //---------------------------------------------------------
-// !! NDEBUG sollte aktiviert werden wenn die Aktorentwicklung und die Tests abgeschlossen sind und das Gerät in den 'Produktionsmodus' geht.
-// Insbesondere die RAM-Einsparungen sind wichtig für die Stabilität / dynamische Speicherzuweisungen etc.
-// Dies beseitigt dann auch die mögliche Arduino-Warnung 'Low memory available, stability problems may occur'.
+// IMPORTANT NOTE: switch on NDEBUG in production phase after havin finished all bring-up tests.
 //
 //#define NDEBUG
 //#define USE_CC1101_ALT_FREQ_86835  //use alternative frequency to compensate not correct working cc1101 modules
@@ -61,21 +57,21 @@
 #define SENS_PIN_3  7           // Monitor     // lila
 #define SENS_PIN_4  9           // Heating     // green
 
-#define CHARGE_CONTROL_PIN  A3                 // control pin for charging the accumulators, active low, the charging software is not yet implemented, please charge the accumulator batteries manually for the time being!
-#define MAX_ACCUMULATOR_VOLTAGE  5100          // limit the accumulators voltage: charging is disabled if the accumulator voltage is above this threshold
+#define CHARGE_CONTROL_PIN         A3          // control pin for charging the accumulators, active low, the charging software is not yet implemented, please charge the accumulator batteries manually for the time being!
+#define MAX_ACCUMULATOR_VOLTAGE    5100        // limit the accumulators voltage: charging is disabled if the accumulator voltage is above this threshold
 
 #define V_RELAIS_BAT_SUPPLIED_PIN  3           // D3
-#define PULSE_WIDTH_V_RELAIS_BAT_SUPPLIED_PIN  millis2ticks(1000UL)               // pulse width at V_RELAIS_BAT_SUPPLIED_PIN, needs to be long enough to switch the AC power relay and to power up the main DC power
+#define PULSE_WIDTH_V_RELAIS_BAT_SUPPLIED_PIN  millis2ticks(900UL)               // pulse width at V_RELAIS_BAT_SUPPLIED_PIN, needs to be long enough to switch the AC power relay and to power up the main DC power
 #define PULSE_WIDTH_CHARGE_CONTROL_PIN         seconds2ticks(60UL * 20 * 0.88)    // pulse width at CHARGE_CONTROL_PIN for an intermediate charging of the accumulator batteris after having switched on a relay output (20 minutes)
                                                                                   // 60 seconds * 15 (= minutes) * corrective factor
 
 #define SABOTAGE_PIN_1    false                // disable SABOTAGE_PIN false=0
 
-#define LED_PIN           4
-#define CONFIG_BUTTON_PIN 8
+#define LED_PIN                  4
+#define CONFIG_BUTTON_PIN        8
 
 // number of available peers per channel
-#define CREATE_INTERNAL_PEERINGS          // needs to be set in order to create the internal peerings
+#define CREATE_INTERNAL_PEERINGS  // needs to be set in order to create the internal peerings
 #define PEERS_PER_SwitchChannel  6
 #define PEERS_PER_SENSCHANNEL    6
 
@@ -90,7 +86,7 @@
 using namespace as;
 
 PulseReset PulseReset_V_RELAIS_BAT_SUPPLIED_PIN;         // see PulseReset.h
-PulseReset PulseReset_CHARGE_CONTROL_PIN;    // see PulseReset.h
+PulseReset PulseReset_CHARGE_CONTROL_PIN;                // see PulseReset.h
 
 // define all device properties
 const struct DeviceInfo PROGMEM devinfo = {
@@ -155,119 +151,126 @@ protected:
    typedef ActorChannel<HalType,SwitchList1,SwitchList3,PeerCount,List0Type,SwitchStateMachine> BaseChannel;
    uint8_t pin;
 
-   uint8_t supply_V_relais_by_battery_pin;
-   uint8_t relay_pin_1,relay_pin_2,relay_pin_3,relay_pin_4;
-   bool    status_relay_1,status_relay_2,status_relay_3,status_relay_4;
    bool    active_external_relay_output_flag;
 
 public:
-      static uint8_t state_AC_supply_relay_1;          // static variable that is shared between all instantiations of derived_SwitchChannel
-
+   static uint8_t state_AC_supply_relay_1;          // static variable that is shared between all instantiations of derived_SwitchChannel
+   static bool    current_relay_states[4];          // static array that iss conserving the relay states and sharing the relays' status between all instantiations of derived_SwitchChannel
+      
 public:
 
   void init (uint8_t p,bool value=false) {
-    //DPRINTLN(F("derived_SwitchChannel init .."));
+
+    PulseReset_V_RELAIS_BAT_SUPPLIED_PIN.init(V_RELAIS_BAT_SUPPLIED_PIN, false);
+    PulseReset_CHARGE_CONTROL_PIN.init(CHARGE_CONTROL_PIN, true);                       // temporary solution as long as the charge control software is not yet implemented
+    
     pin=p;
-    //DPRINT(F("Initializing relay pin : ")); DDEC( pin ); DPRINT(" "); DDECLN( value );
     IODriver::setOutput(pin);
     this->setflag(value,LOWACTIVE);
     typename BaseChannel::List1 l1 = BaseChannel::getList1();
     this->set(l1.powerUpAction() == true ? 200 : 0, 0, 0x0000 );  // 0x0000 defines power-up status of the relays
     this->changed(true);
-    //DPRINTLN(F("Initializing of pin done "));
-    supply_V_relais_by_battery_pin = V_RELAIS_BAT_SUPPLIED_PIN;
-    DPRINT(F("Initializing V_relais control pin : ")); DDECLN( supply_V_relais_by_battery_pin );
-    IODriver::setOutput(supply_V_relais_by_battery_pin);  // set D3 as an output pin
-    IODriver::setHigh(supply_V_relais_by_battery_pin);    // low-active output, therefore the inactive state is High
+    IODriver::setOutput(V_RELAIS_BAT_SUPPLIED_PIN);  // set D3 as an output pin
+    IODriver::setHigh(V_RELAIS_BAT_SUPPLIED_PIN);    // low-active output, therefore the inactive state is High
 
     // charge_control_pin
     // put temporarily here until the charging class will be implemented later on
     IODriver::setOutput(CHARGE_CONTROL_PIN);  // set A3 as an output pin
     IODriver::setLow(CHARGE_CONTROL_PIN);     // low-active output, short pulse during init
-    delay(400);                                  // short 400 ms pulse of the blue LED during startup
+    delay(200);                               // short 200 ms pulse of the blue LED during startup
     IODriver::setHigh(CHARGE_CONTROL_PIN);    // low-active output, therefore the inactive state is High
     IODriver::setInput(CHARGE_CONTROL_PIN);   // set A3 as an input pin in order to prevent leakage currents through R9
-    
-    // define the Arduino control pins for the four relays
-    relay_pin_1 = RELAY_PIN_1;
-    relay_pin_2 = RELAY_PIN_2;
-    relay_pin_3 = RELAY_PIN_3;
-    relay_pin_4 = RELAY_PIN_4;
 
-    PulseReset_V_RELAIS_BAT_SUPPLIED_PIN.init(supply_V_relais_by_battery_pin, false);
-    PulseReset_CHARGE_CONTROL_PIN.init(CHARGE_CONTROL_PIN, true);                       // temporary solution
   }
+
+
+  void store_relay_state_in_static_array ( uint8_t p, uint8_t newstate ) {
+    switch(p) {
+      case RELAY_PIN_1:
+        current_relay_states[0] =  newstate == AS_CM_JT_ON ? true : false ;  // set to true if newstate is equal to AS_CM_JT_ON
+        break;
+      case RELAY_PIN_2:
+        current_relay_states[1]  = newstate == AS_CM_JT_ON ? true : false ;
+        break;
+      case RELAY_PIN_3:
+        current_relay_states[2]  = newstate == AS_CM_JT_ON ? true : false ;
+        break;
+      case RELAY_PIN_4:
+        current_relay_states[3]  = newstate == AS_CM_JT_ON ? true : false ;
+        break;
+      default:
+        // wrong pin number; issue error message in serial monitor
+        //DPRINT(F("Wrong pin in store_relay_state_in_static_array : ")); DDEC( pin ); DPRINTLN(F(" !"));
+        break;
+    }
+  }
+  
 
   bool check_if_any_external_relay_output_is_active() {
     
-     active_external_relay_output_flag = false;
-
-     status_relay_1 = IODriver::getState(relay_pin_1); 
-     status_relay_2 = IODriver::getState(relay_pin_2); 
-     status_relay_3 = IODriver::getState(relay_pin_3);
-     status_relay_4 = IODriver::getState(relay_pin_4);
-    
-     
-     DPRINT(F("Relay 1 status : ")); DDECLN( status_relay_1 );
-     DPRINT(F("Relay 2 status : ")); DDECLN( status_relay_2 );
-     DPRINT(F("Relay 3 status : ")); DDECLN( status_relay_3 );
-     DPRINT(F("Relay 4 status : ")); DDECLN( status_relay_4 );
-
-     active_external_relay_output_flag = !(status_relay_2 && status_relay_3 && status_relay_4); // set true if at least one external relay output is active (active = 0 since low-active outputs)
-                                                                                                // relay_pin_1 is not an external relay output ! AC power is an internal power switch only!
-     
-     DPRINT(F("active_external_relay_output_flag status : ")); DDECLN( active_external_relay_output_flag );
-     
-     return active_external_relay_output_flag;
+    active_external_relay_output_flag = false;
+    active_external_relay_output_flag = (current_relay_states[1] || current_relay_states[2] || current_relay_states[3]); // set true if at least one external relay output is active (state == AS_CM_JT_ON)                                                                                                                         // current_relay_states[0] is not an external relay output of the hardware ! AC power is an internal power switch only
+    return active_external_relay_output_flag;
   }
 
   void pulse_charging_of_accumulators () {
     IODriver::setOutput(CHARGE_CONTROL_PIN);  // set A3 as an output pin
     IODriver::setLow(CHARGE_CONTROL_PIN);     // low-active output, short pulse during init
-    DPRINT(F("Charging accumulators for ")); DDEC( PULSE_WIDTH_CHARGE_CONTROL_PIN ); DPRINTLN(F(" milliseconds"));
     PulseReset_CHARGE_CONTROL_PIN.reset_pins(PULSE_WIDTH_CHARGE_CONTROL_PIN); // stop charging after PULSE_WIDTH_CHARGE_CONTROL_PIN milliseconds
   }
 
   virtual void switchState(__attribute__((unused)) uint8_t oldstate,uint8_t newstate,__attribute__((unused)) uint32_t tdelay) {
 
-    DPRINT(F("\nWorking on relay pin : ")); DDECLN( pin );
+    /*DPRINT(F("\nWorking on relay pin : ")); DDECLN( pin );
     DPRINT(F("Old State : ")); DDECLN( oldstate );
     DPRINT(F("New State : ")); DDECLN( newstate );
+    if ( newstate == AS_CM_JT_ON ) {
+       DPRINTLN(F("New State : ON")); 
+    } else
+    if ( newstate == AS_CM_JT_OFF ) {
+       DPRINTLN(F("New State : OFF")); 
+    }
+    else {
+       DPRINTLN(F("New State : OTHER")); 
+    }
     
+    */
     if ( ( newstate == AS_CM_JT_ON ) || ( newstate == AS_CM_JT_OFF ) ) {
+
+      store_relay_state_in_static_array(pin, newstate);  // store new state of channel in the shared static array 'current_relay_states'
+      
       // set V_RELAIS_BAT_SUPPLIED_PIN to the active low state for a temporary supply of V_relais by the batteries
-      DPRINTLN(F("Set V_RELAIS_BAT_SUPPLIED_PIN to active .."));
-      IODriver::setLow(supply_V_relais_by_battery_pin);  // active low output!
+      IODriver::setOutput(V_RELAIS_BAT_SUPPLIED_PIN);  // set D3 as an output pin
+      IODriver::setLow(V_RELAIS_BAT_SUPPLIED_PIN);     // active low output!
     }
    
-    DPRINT(F("Switching relay pin : ")); DDEC( pin ); DPRINT(F(" ")); DDECLN( newstate );
     if( newstate == AS_CM_JT_ON ) {
-      
+      IODriver::setOutput(pin); // define pin as output (again)
       if( this->hasflag(LOWACTIVE) == true ) IODriver::setLow(pin);
       else IODriver::setHigh(pin);
 
       // switch on the AC supply relay for supplying the relays from now on
-      DPRINTLN(F("Switching on AC supply relay .."));
+      IODriver::setOutput(RELAY_PIN_1);
       IODriver::setLow(RELAY_PIN_1);
 
-      // charge the accumulators for a pulse of PULSE_WIDTH_CHARGE_CONTROL_PIN milliseconds
+      // charge the accumulators for a pulse of PULSE_WIDTH_CHARGE_CONTROL_PIN ticks
       pulse_charging_of_accumulators();
     }
     else if ( newstate == AS_CM_JT_OFF ) {
+      IODriver::setOutput(pin); // define pin as output (again)
       if( this->hasflag(LOWACTIVE) == true ) {
         IODriver::setHigh(pin);
+        IODriver::setInput(pin);  // for avoiding any leakage
       }
       else IODriver::setLow(pin);
     }
 
     // save the current status of the AC power relay at pin RELAY_PIN_1 in the static variable RELAY_PIN_1; this variable caan be read by the other derived_SwitchChannel instantiations
-    if ( pin == relay_pin_1 ) {
-      DPRINT(F("Updating  AC power state 'state_AC_supply_relay_1' : ")); DDECLN( newstate );
+    if ( pin == RELAY_PIN_1 ) {
       state_AC_supply_relay_1 = newstate;   // store the new state of AC supply relay at pin RELAY_PIN_1 in static variable state_AC_supply_relay_1
     }
 
     if ( ( newstate == AS_CM_JT_ON ) || ( newstate == AS_CM_JT_OFF ) ) {
-      DPRINT(F("delay for ")); DDEC( PULSE_WIDTH_V_RELAIS_BAT_SUPPLIED_PIN ); DPRINTLN(F(" ms at V_RELAIS_BAT_SUPPLIED_PIN"));
       PulseReset_V_RELAIS_BAT_SUPPLIED_PIN.reset_pins(PULSE_WIDTH_V_RELAIS_BAT_SUPPLIED_PIN); // reset V_RELAIS_BAT_SUPPLIED_PIN to inactive state after a PULSE_WIDTH_V_RELAIS_BAT_SUPPLIED_PIN miliseconds delay in order to get main DC supply switched on and stabilized
     }
     
@@ -277,11 +280,11 @@ public:
     if ( ! active_external_relay_output_flag) {
       // switch off the AC suppy relay if no external relay output is active and ( state_AC_supply_relay_1 == AS_CM_JT_OFF )
       if (state_AC_supply_relay_1 == AS_CM_JT_OFF) {
-        DPRINTLN(F("Switching off AC supply relay .."));
+        IODriver::setOutput(RELAY_PIN_1);
         IODriver::setHigh(RELAY_PIN_1); // reset state of AC supply to the 'off'-state: active low-output! only if ( state_AC_supply_relay_1 == AS_CM_JT_OFF )
+        IODriver::setInput(RELAY_PIN_1); 
       }
       else {
-        DPRINTLN(F("Keep state of AC supply relay unchanged"));
       }
       
     }
@@ -390,6 +393,9 @@ ConfigButton<MixDevice> cfgBtn(sdev);
 
 template <class HalType,int PeerCount,class List0Type,class IODriver>                                     // <-------- ALLOCATE MEMORY FOR THE STATIC VARIABLE - see also https://stackoverflow.com/questions/17071130/default-argument-for-template-parameter-for-class-enclosing
 uint8_t derived_SwitchChannel< HalType, PeerCount, List0Type, IODriver>::state_AC_supply_relay_1;         // <-------- ALLOCATE MEMORY FOR THE STATIC VARIABLE -
+template <class HalType,int PeerCount,class List0Type,class IODriver>                                                                  // <-------- ALLOCATE MEMORY FOR THE STATIC VARIABLE - see also https://stackoverflow.com/questions/17071130/default-argument-for-template-parameter-for-class-enclosing
+bool derived_SwitchChannel< HalType, PeerCount, List0Type, IODriver>::current_relay_states[4] = {false, false, false, false};          // <-------- ALLOCATE MEMORY FOR THE STATIC VARIABLE -
+
 
 
 void initPeerings (bool first) {
@@ -419,27 +425,26 @@ void setup () {
   //first=true;    // comment out for only one (!) programming if the peering is not working as expected, e.g. if after you have changed the pin assignment
   
   DPRINTLN(F("Init HAL .."));
-  
+
+
   sdev.derived_switchChannel(1).init(RELAY_PIN_1, true);   // inverse polarity see https://homematic-forum.de/forum/viewtopic.php?t=53687
   sdev.derived_switchChannel(2).init(RELAY_PIN_2, true);
   sdev.derived_switchChannel(3).init(RELAY_PIN_3, true);
   sdev.derived_switchChannel(4).init(RELAY_PIN_4, true);
-  
+
   sdev.sensorChannel(5).init(SENS_PIN_1, SABOTAGE_PIN_1);  // sabotage pin are disabled; see above
   sdev.sensorChannel(6).init(SENS_PIN_2, SABOTAGE_PIN_1);
   sdev.sensorChannel(7).init(SENS_PIN_3, SABOTAGE_PIN_1);
   sdev.sensorChannel(8).init(SENS_PIN_4, SABOTAGE_PIN_1);
 
-  DPRINTLN(F("Init HAL done"));
   
   buttonISR(cfgBtn, CONFIG_BUTTON_PIN);
 
- 
+
   sdev.derived_switchChannel(RELAY_PIN_1).switchState(AS_CM_JT_ON, AS_CM_JT_OFF, 0);  // switch off the relay as initial state
   sdev.derived_switchChannel(RELAY_PIN_2).switchState(AS_CM_JT_ON, AS_CM_JT_OFF, 0);  // switch off the relay as initial state
   sdev.derived_switchChannel(RELAY_PIN_3).switchState(AS_CM_JT_ON, AS_CM_JT_OFF, 0);  // switch off the relay as initial state
   sdev.derived_switchChannel(RELAY_PIN_4).switchState(AS_CM_JT_ON, AS_CM_JT_OFF, 0);  // switch off the relay as initial state
- 
 
   sdev.radio().initReg(CC1101_PATABLE, PA_Normal);                                    // normal RF transmission power of CC1101, PA_Normal = 0x50, can be set to PA_LowPower or PA_MaxPower; see Radio.h
 
