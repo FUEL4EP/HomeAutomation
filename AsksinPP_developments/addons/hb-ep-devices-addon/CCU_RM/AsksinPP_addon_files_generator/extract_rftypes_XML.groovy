5104488df658b5ca427b5820394f25f0d8cbabf8
FUEL4EP <epre@unitybox.de>
2024-06-04 12:10:59 +0200

    update of AsksinPP_developments/addons/hb-ep-devices-addon/CCU_RM/AsksinPP_addon_files_generator/extract_rftypes_XML.groovy due to change of EQ3's libraries

--- AsksinPP_developments/addons/hb-ep-devices-addon/CCU_RM/AsksinPP_addon_files_generator/extract_rftypes_XML.groovy
+++ AsksinPP_developments/addons/hb-ep-devices-addon/CCU_RM/AsksinPP_addon_files_generator/extract_rftypes_XML.groovy
@@ -113,7 +113,7 @@ class extract_rftypes_XML {
         println "\n.. reading '${file_name}' .."
         def inputFile = new File(file_name)
         
-        String required_match_regex = /^jQuery\.extend\(true\,langJSON\,\ \{\n\ \ \"de\"\ \:\ [\s\S]*langJSON.de.stringTableValveStateErrorPosition;[\s\S]$/
+        String required_match_regex = /^jQuery\.extend\(true\,langJSON\,\ \{\n\ \ \"de\"\ \:\ [\s\S]*langJSON.de.stringTableValveStateErrorPosition;$/
 
 
         if ( !( inputFile.text ==~ required_match_regex) )
